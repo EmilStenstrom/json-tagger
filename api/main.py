8308c436e3dd410cec8c13481d7bde9f1d39808f
Emil Stenström <em@kth.se>
2014-12-27 00:49:06 +0100

    Refactoring to make code easier to read.

--- api/main.py
+++ api/main.py
@@ -1,41 +1,39 @@
+from bottle import route, request, response, run, view
 from collections import OrderedDict
-from server import prepare_data, query_server
 from parser import parse_response
-from bottle import route, request, run, view, JSONPlugin, json_dumps as dumps
-from functools import partial
-
+from server import query_server
 import bottle
-bottle.TEMPLATE_PATH = ["api/views/"]
-bottle.debug(True)
-bottle.TEMPLATES.clear()
-
-better_dumps = partial(dumps, separators=(',', ':'))
-bottle.default_app().uninstall(JSONPlugin)
-bottle.default_app().install(JSONPlugin(better_dumps))
+import json
 
 @route('/api/')
-@view('index')
+@view('api/views/index')
 def index():
     site = "%s://%s" % (request.urlparts.scheme, request.urlparts.netloc)
     return {"site": site}
 
 @route('/api/tag', method=["get", "post"])
 def tag():
-    data = request.POST.get("data", None)
-
-    if not data:
-        data = request.body.getvalue()
+    # Support posting data both via forms and via POST body
+    data = request.POST.get("data", request.body.getvalue())
 
     if not data:
         return {"error": "No data posted"}
 
-    data = prepare_data(data)
-    response = query_server(data)
-    sentences, entities = parse_response(response)
+    raw_text = query_server(data)
+    sentences, entities = parse_response(raw_text)
 
-    return OrderedDict([
+    response.content_type = "application/json"
+
+    pretty = request.POST.get("pretty", False)
+    json_kwargs = {"separators": (',', ':')}
+    if pretty:
+        json_kwargs = {"indent": 4, "separators": (', ', ': ')}
+
+    return json.dumps(OrderedDict([
         ("sentences", sentences),
         ("entities", entities),
-    ])
+    ]), **json_kwargs)
 
+bottle.debug(True)
+bottle.TEMPLATES.clear()
 run(host='localhost', port=8000, reloader=True)
