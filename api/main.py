cf9dfd33fc2d3fee4ebc0073f21c3c9fa18c47b8
Emil Stenström <em@kth.se>
2014-12-26 18:49:02 +0100

    Move everything to api app. Add frontpage.

--- api/main.py
+++ api/main.py
@@ -1,22 +1,30 @@
 from collections import OrderedDict
 from server import prepare_data, query_server
 from parser import parse_response
-from bottle import route, request, run
+from bottle import route, request, run, view
+
+import bottle
+bottle.TEMPLATE_PATH = ["api/views/"]
+bottle.debug(True)
+bottle.TEMPLATES.clear()
 
 @route('/api/')
+@view('index')
 def index():
-    return '''
-        <form action="/api/tag" method="post">
-            <textarea name="data"></textarea>
-            <button type="submit">Tagga</button>
-        </form>
-    '''
+    site = "%s://%s" % (request.urlparts.scheme, request.urlparts.netloc)
+    return {"site": site}
 
 @route('/api/tag', method=["get", "post"])
 def tag():
-    if not request.POST:
+    data = request.POST.get("data", None)
+
+    if not data:
+        data = request.body.getvalue()
+
+    if not data:
         return {"error": "No data posted"}
-    data = prepare_data(request.POST["data"])
+
+    data = prepare_data(data)
     response = query_server(data)
     sentences, entities = parse_response(response)
 
