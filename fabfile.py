777926aff5814dd067a8f68dfd593745ba592047
Emil Stenström <em@kth.se>
2014-12-30 20:53:08 +0100

    Move Stagger to separate app.

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,6 @@
 from fabric.operations import local
 
-def runtagger():
-    local("java -jar stagger/stagger.jar -modelfile models/swedish.bin -server 127.0.0.1 9000")
-
-def runapi():
+def run():
     local("ENVIRONMENT=development python api/main.py")
 
 def deploy():
