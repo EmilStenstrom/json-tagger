89ba64712dd4b9fb725557cd671d4bd48a46445e
Emil Stenström <em@kth.se>
2015-12-06 18:57:10 +0100

    Replace external Stagger with internal efselab tagger. Upgrade project to Python3.
    Former-commit-id: e93e0ec8a3a3ef1c3ba8fedd1475386388d79c7d

--- api/main.py
+++ api/main.py
@@ -1,11 +1,11 @@
-from bottle import route, request, response, view
-from collections import OrderedDict
-from api.parser import parse_response
-from api.server import query_server
 import json
+from collections import OrderedDict
+from itertools import groupby
 
-STAGGER_ADDRESS = "mumin.ling.su.se"
-STAGGER_PORT = 46502
+from bottle import route, request, response, view
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+from efselab import suc as tagger
 
 @route('/')
 @view('api/views/index')
@@ -16,13 +16,43 @@ def index():
 @route('/tag', method=["get", "post"])
 def tag():
     # Support posting data both via forms and via POST body
-    data = request.POST.get("data", request.body.getvalue())
+    data = request.POST.getunicode("data", str(request.body.getvalue(), "utf-8"))
 
     if not data:
         return {"error": "No data posted"}
 
-    raw_text = query_server(data, STAGGER_ADDRESS, STAGGER_PORT)
-    sentences, entities = parse_response(raw_text)
+    weights = None
+    with open('efselab/suc.bin', 'rb') as f:
+        weights = f.read()
+
+    sentence_list = sent_tokenize(data, "swedish")
+    sentences = []
+    entities = []
+    for j, sentence in enumerate(sentence_list):
+        tokens = word_tokenize(sentence, "swedish")
+        tags = tagger.tag(weights, tokens)
+        tokens_and_tags = tuple(zip(tokens, tags))
+
+        sentence_data = []
+        for i, (word, features) in enumerate(tokens_and_tags):
+            token_data = OrderedDict([
+                ("word_index", str(i + 1)),
+                ("word_form", word),
+                ("pos_tag", features.split("|")[0]),
+                ("morph_feat", "|".join(features.split("|")[1:]) or None),
+                ("token_id", "tok:{j}:{i}".format(j=j, i=i)),
+            ])
+            sentence_data.append(token_data)
+
+        grouped_tokens = groupby(sentence_data, lambda token: token["pos_tag"])
+        for token_group in [list(v) for k, v in grouped_tokens if k == "PM"]:
+            entity_data = OrderedDict([
+                ("word_form", " ".join([token["word_form"] for token in token_group])),
+                ("token_ids", [token["token_id"] for token in token_group]),
+            ])
+            entities.append(entity_data)
+
+        sentences.append(sentence_data)
 
     response.content_type = "application/json"
 
--- runner.py
+++ runner.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
     environment = os.environ.get("ENVIRONMENT", None)
     assert environment, "Needs $ENVIRONMENT variable set"
     if environment == "development":
-        print "RUNNING IN DEVELOPMENT MODE"
+        print("RUNNING IN DEVELOPMENT MODE")
         bottle.debug(True)
         bottle.TEMPLATES.clear()
         run(host='localhost', port=8000, reloader=True)
     elif environment == "production":
-        print "RUNNING IN PRODUCTION MODE"
+        print("RUNNING IN PRODUCTION MODE")
         run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
     else:
         assert False, "That's not a valid $ENVIRONMENT"
