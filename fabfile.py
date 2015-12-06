bc6adea8986b44a9a3107834cf7380d7c7d8dd8d
Emil Stenström <em@kth.se>
2015-12-06 18:47:20 +0100

    Replace fabric with some simple calls to argparse.

--- fabfile.py
+++
@@ -1,7 +0,0 @@
-from fabric.operations import local
-
-def run():
-    local("ENVIRONMENT=development python runner.py")
-
-def deploy():
-    local("git push heroku master")
---
+++ run.py
@@ -0,0 +1,13 @@
+import os
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--run', action="store_true")
+parser.add_argument('--deploy', action="store_true")
+args = parser.parse_args()
+
+if not any(vars(args).values()):
+    parser.print_help()
+elif args.run:
+    os.system("ENVIRONMENT=development python runner.py")
+elif args.deploy:
+    os.system("git push heroku master")
