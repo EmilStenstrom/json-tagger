e4a3be1b8cc039ac4952575cebcb5cebca69abb8
Emil Stenström <em@kth.se>
2015-12-07 21:41:41 +0100

    Remove explicit utf-8, it breaks some posts.
    Former-commit-id: 49f76f7715e901fc4b5005600e95e45fc66be112

--- api/main.py
+++ api/main.py
@@ -19,7 +19,7 @@ def index():
 @route('/tag', method=["get", "post"])
 def tag():
     # Support posting data both via forms and via POST body
-    data = request.POST.getunicode("data", str(request.body.getvalue(), "utf-8"))
+    data = request.POST.getunicode("data", str(request.body.getvalue()))
 
     if not data:
         return {"error": "No data posted"}
