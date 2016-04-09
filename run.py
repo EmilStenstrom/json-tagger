60a10e8fbfd40197db8226f0791c7064c80fe370
Emil Stenström <em@kth.se>
2016-04-09 19:34:49 +0200

    Add new update command that updates efselab dependencies.
    Former-commit-id: 6cfed1b9af9c0bbf34b7e58e3aa8ac3bada85aa7

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
+import sys
 import os
 import argparse
+import shutil
+
+from efselab import build
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--run', action="store_true")
 parser.add_argument('--deploy', action="store_true")
+parser.add_argument('--update', action="store_true")
 args = parser.parse_args()
 
 if not any(vars(args).values()):
@@ -11,3 +17,20 @@ elif args.run:
     os.system("ENVIRONMENT=development python server.py")
 elif args.deploy:
     os.system("git push heroku master")
+elif args.update:
+    if not os.path.exists("../efselab/"):
+        sys.exit("Couldn't find a local efselab checkout...")
+
+    shutil.copy("../efselab/fasthash.c", "./efselab")
+    shutil.copy("../efselab/lemmatize.c", "./efselab")
+    shutil.copy("../efselab/pysuc.c", "./efselab/suc.c")
+
+    if not os.path.exists("../efselab/swe-pipeline"):
+        sys.exit("Couldn't find a local swe-pipeline directory for models...")
+
+    shutil.copy("../efselab/swe-pipeline/suc.bin", "./efselab")
+    shutil.copy("../efselab/swe-pipeline/suc-saldo.lemmas", "./efselab")
+
+    print("Building new files...")
+    os.chdir("efselab")
+    build.main()
