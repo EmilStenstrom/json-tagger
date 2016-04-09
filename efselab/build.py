75d6e88de0ed8f8cb081de15ce0d3949a78c9ded
Emil Stenström <em@kth.se>
2016-04-09 19:34:29 +0200

    Put module in method to enable calls from libraries.
    Former-commit-id: e614cec07ee71723be5b114163fe835961f6811c

--- efselab/build.py
+++ efselab/build.py
@@ -3,17 +3,21 @@ from distutils.core import setup, Extension
 
 MODULES_TO_BUILD = ["fasthash", "suc", "lemmatize"]
 
-for module in MODULES_TO_BUILD:
-    setup(
-        name=module,
-        ext_modules=[
-            Extension(
-                name=module,
-                sources=['%s.c' % module],
-                libraries=[],
-                extra_compile_args=['-Wall', '-Wno-unused-function'],
-                extra_link_args=[]
-            )
-        ],
-        script_args=['build_ext', '--inplace']
-    )
+def main():
+    for module in MODULES_TO_BUILD:
+        setup(
+            name=module,
+            ext_modules=[
+                Extension(
+                    name=module,
+                    sources=['%s.c' % module],
+                    libraries=[],
+                    extra_compile_args=['-Wall', '-Wno-unused-function'],
+                    extra_link_args=[]
+                )
+            ],
+            script_args=['build_ext', '--inplace']
+        )
+
+if __name__ == '__main__':
+    main()
