7374435fc695b0a28b558b6b8fec8a88ea602de4
Emil Stenström <em@kth.se>
2017-04-08 16:36:58 +0200

    Redirect to https everywhere.
    Former-commit-id: 0decc0069eaea18bc5faf173318045c29d604d3e

---
+++ middleware.py
@@ -0,0 +1,28 @@
+import re
+
+import falcon
+
+_FORWARDED_PROTO_RE = re.compile('proto=([A-Za-z]+)')
+
+class RedirectToHTTPS(object):
+    def process_request(self, request, response):
+        if request.host in ["localhost", "127.0.0.1"]:
+            return
+
+        if request.protocol.lower() == 'https':
+            return
+
+        xfp = request.get_header('X-FORWARDED-PROTO')
+        if xfp and xfp.lower() == 'https':
+            return
+
+        forwarded = request.get_header('FORWARDED')
+        if forwarded:
+            first, __, __ = forwarded.partition(',')
+
+            match = _FORWARDED_PROTO_RE.search(first)
+            if match and match.group(1).lower() == 'https':
+                return
+
+        site_https = request.uri.replace("http://", "https://", maxreplace=1)
+        raise falcon.HTTPMovedPermanently(site_https)
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import falcon
 from api.api_resource import ApiResource
 from api.docs_resource import DocsResource
+from middleware import RedirectToHTTPS
 
-app = falcon.API()
+app = falcon.API(middleware=[RedirectToHTTPS()])
 app.add_route('/', DocsResource())
 app.add_route('/tag', ApiResource())
