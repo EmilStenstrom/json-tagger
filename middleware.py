dae168b6da1b28d1b7b3b3586855edf0394f2942
Emil Stenström <em@kth.se>
2019-01-06 10:23:41 +0100

    Remove RedirectToHTTPS and let Cloudflare handle HTTPS instead.

--- middleware.py
+++ middleware.py
@@ -4,29 +4,6 @@ import falcon
 
 _FORWARDED_PROTO_RE = re.compile('proto=([A-Za-z]+)')
 
-class RedirectToHTTPS:
-    def process_request(self, request, response):
-        if request.host in ["localhost", "127.0.0.1"]:
-            return
-
-        if request.protocol.lower() == 'https':
-            return
-
-        xfp = request.get_header('X-FORWARDED-PROTO')
-        if xfp and xfp.lower() == 'https':
-            return
-
-        forwarded = request.get_header('FORWARDED')
-        if forwarded:
-            first, __, __ = forwarded.partition(',')
-
-            match = _FORWARDED_PROTO_RE.search(first)
-            if match and match.group(1).lower() == 'https':
-                return
-
-        site_https = request.uri.replace("http://", "https://", 1)
-        raise falcon.HTTPMovedPermanently(site_https)
-
 class RedirectToComDomain:
     def process_request(self, request, response):
         if request.host != "json-tagger.herokuapp.com":
--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import falcon
 from api.api_resource import ApiResource
 from api.docs_resource import DocsResource
-from middleware import RedirectToHTTPS, RedirectToComDomain
+from middleware import RedirectToComDomain
 
-app = falcon.API(middleware=[RedirectToHTTPS(), RedirectToComDomain()])
+app = falcon.API(middleware=[RedirectToComDomain()])
 app.add_route('/', DocsResource())
 app.add_route('/tag', ApiResource())
