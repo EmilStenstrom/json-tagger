711b01a0c5948d506aa612e0d36510ef175a71b5
Emil Stenström <em@kth.se>
2019-01-07 07:35:34 +0100

    Make run.py use functions for clarity.

--- run.py
+++ run.py
@@ -2,14 +2,18 @@
 import os
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--run', action="store_true")
-parser.add_argument('--deploy', action="store_true")
-args = parser.parse_args()
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--run', action="store_true")
+	parser.add_argument('--deploy', action="store_true")
+	args = parser.parse_args()
 
-if not any(vars(args).values()):
-    parser.print_help()
-elif args.run:
-    os.system("gunicorn server:app --reload --config gunicorn_config.py")
-elif args.deploy:
-    os.system("git push heroku master")
+	if not any(vars(args).values()):
+	    parser.print_help()
+	elif args.run:
+	    os.system("gunicorn server:app --reload --config gunicorn_config.py")
+	elif args.deploy:
+	    os.system("git push heroku master")
+
+if __name__ == '__main__':
+	main()
