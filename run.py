57b0ea6aeaebac81b1aef938d54bf88cfd957d3d
Emil Stenström <em@kth.se>
2019-01-11 12:33:35 +0100

    Use spaces, not tabs.

--- run.py
+++ run.py
@@ -3,26 +3,26 @@ import os
 import argparse
 
 def run():
-	""" Reuse the Procfile to start the dev server """
-	with open("Procfile", "r") as f:
-		command = f.read().strip()
+    """ Reuse the Procfile to start the dev server """
+    with open("Procfile", "r") as f:
+        command = f.read().strip()
 
-	command = command.replace("web: ", "")
-	command = command.replace("--preload", "--reload")
-	os.system(command)
+    command = command.replace("web: ", "")
+    command = command.replace("--preload", "--reload")
+    os.system(command)
 
 def deploy():
-	os.system("git push dokku master")
+    os.system("git push dokku master")
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--deploy', action="store_true", required=False)
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--deploy', action="store_true", required=False)
+    args = parser.parse_args()
 
-	if args.deploy:
-	    deploy()
-	else:
-		run()
+    if args.deploy:
+        deploy()
+    else:
+        run()
 
 if __name__ == '__main__':
-	main()
+    main()
