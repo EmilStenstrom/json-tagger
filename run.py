9bc7d09e9abf79f6af7f7fd3cdddbfacd91ba9d3
Emil Stenström <em@kth.se>
2020-08-01 15:02:42 +0200

    Add command to update dependencies.

--- run.py
+++ run.py
@@ -14,15 +14,24 @@ def run():
 def deploy():
     os.system("git push dokku master")
 
+def dependencies():
+    os.system("pip-compile --upgrade requirements.in")
+    os.system("pip-compile --upgrade requirements-dev.in")
+    os.system("pip-sync requirements-dev.txt")
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--deploy', action="store_true", required=False)
+    parser.add_argument('--deps', action="store_true", required=False)
     args = parser.parse_args()
 
     if args.deploy:
         deploy()
+    elif args.deps:
+        dependencies()
     else:
         run()
+
 
 if __name__ == '__main__':
     main()
